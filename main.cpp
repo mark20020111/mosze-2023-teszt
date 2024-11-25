4fe007c00e8092113e4c43ed996d7f652f97c197
mark20020111 <kelemen.mark@hallgato.sze.hu>
2024-11-25 14:50:57 +0100

    Hibák felkommentelve

--- main.cpp
+++ main.cpp
@@ -4,23 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //rosszul van írva az N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //hiányzik a pontosvessző
+    for (int i = 0;) //hiányzik a ciklusfeltétel
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //Az i-hez hozzá kell adni 1et hogy 1től kezdjünk
     }
     for (int i = 0; i; i++)
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //az átlagos érdemes 0ra inicializálni
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    //a lefoglalt memóriát törölni kell delete[] b
+
     return 0;
 }
